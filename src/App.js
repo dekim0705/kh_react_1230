ed5b3ff9ec92eaaa3e1fc7676ac304264fb6282c
dekim0705 <dekim0705@gmail.com>
2023-04-11 18:26:10 +0900

    4/10/Mon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+function App() {
+
+  const member ={
+    name : "Meredith",
+    job : "General Surgeon",
+    position : "Chief",
+    addr : "Seattle"
+  };
+
+  // eslint-disable-next-line
+  function getGreeting(user) {
+    // return (user) ? (<h1>Welcome, {member.name}.</h1>) : (<h1>Welcome, Visitor.</h1>)
+    if(user) {
+      return <h1>Welcome, {member.name}.</h1>
+    }
+    return <h1>Welcome, Visitor.</h1>
+  };
+  return (
+    // in JSX, class keyword needs to be renamed as className
+    // use of id is NOT recommended to increase the reusability
+    <div className="App">
+      <div>{getGreeting(true)}</div>
+      <p className="title-name">Hi, I am {member.name}.</p>
+      <p className="title-name">I am a {member.position} {member.job}.</p>
+      <p className="title-name">I live in {member.addr}.</p>
+    </div>
+  );
+
+}
+
+export default App;
+/*
+// DEFAULT
 function App() {
   return (
     <div className="App">
@@ -21,5 +54,4 @@ function App() {
     </div>
   );
 }
-
-export default App;
+*/
